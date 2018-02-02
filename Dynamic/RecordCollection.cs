a1db810f33c6739090053748711f833a26afb2d6
Giulio <giuliohome@gmail.com>
2018-02-02 16:14:05 +0100

    data storytelling

---
+++ Dynamic/RecordCollection.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows.Media;
+using System.Linq;
+
+namespace Dynamic
+{
+class RecordCollection:ObservableCollection<Record>
+    {
+     
+        public RecordCollection(List<Bar> barvalues)
+        {
+            Random rand = new Random();
+            BrushCollection brushcoll=new BrushCollection();
+            double scale = barvalues.Max(b=>b.Value);
+            foreach (Bar barval in barvalues)
+            {
+                int num = rand.Next(brushcoll.Count/3);
+                Add(new Record(barval.Value,brushcoll[num], barval.BarName,scale));               
+            }            
+        }
+       
+    }
+
+    class BrushCollection:ObservableCollection<Brush>
+    {
+        public BrushCollection()
+        {
+            Type _brush = typeof(Brushes);
+            PropertyInfo []props=  _brush.GetProperties();
+            foreach (PropertyInfo prop in props)
+            {
+                Brush _color = (Brush)prop.GetValue(null, null);
+                if ( _color!=Brushes.LightSteelBlue && _color != Brushes.White && 
+                     _color != Brushes.WhiteSmoke && _color!=Brushes.LightCyan &&      
+                     _color!=Brushes.LightYellow && _color!=Brushes.Linen)
+                Add(_color);
+            }
+        }
+    }
+
+    class Bar
+    {
+       
+        public string BarName { set; get; }
+
+        public double Value { set; get; }
+
+    }
+	
+    class Record:INotifyPropertyChanged
+    {
+        public Brush Color { set; get; }
+
+        public string Name { set; get; }
+
+        private double _data;
+        public double Data
+        {
+            set
+            {
+                if (_data != value)
+                {
+                    _data = value;
+                  
+                }
+            }
+            get
+            {
+                return _data;
+            }
+        }
+        internal double scale;
+        internal const double maxheight = 200;
+        public double ScaledData
+        {
+        	get { return maxheight*_data/scale;}
+        	private set {}
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Record(double value,Brush color,string name, double barscale)
+        {
+            Data = value;
+            Color = color;
+            Name = name;
+            scale = barscale;
+        }
+
+        protected void PropertyOnChange(string propname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propname));
+            }
+        }
+    }
+}
--- Dynamic/Window1.xaml.cs
+++ Dynamic/Window1.xaml.cs
@@ -20,9 +20,32 @@ namespace Dynamic
 		public Window1()
 		{
 			InitializeComponent();
+			
+			#region Graph
+			List<Bar> _bar = new List<Bar>();
+			
+			_bar.Add(new Bar() { BarName=DateTime.Today.AddMonths(-1).ToString("dd MMM yy"),Value=2200.5});
+			_bar.Add(new Bar() { BarName = DateTime.Today.AddDays(-20).ToString("dd MMM yy"), Value = 1196.6 });
+			_bar.Add(new Bar() { BarName = DateTime.Today.AddDays(-8).ToString("dd MMM yy"), Value = 253.1 });
+			_bar.Add(new Bar() { BarName = DateTime.Today.AddDays(-5).ToString("dd MMM yy"), Value = 808.8 });
+			
+			this.DataContext = new RecordCollection(_bar);
+			animate_Completed();
+			#endregion
 		}
+		
+		#region Graph
+		
+		#endregion
+		
+		
+		#region My OpenCloseMenu
 		bool isOpen = true;
 		void btnOpen_Click(object sender, RoutedEventArgs e)
+		{
+			btnOpen_Click();
+		}
+		void btnOpen_Click()
 		{
 			btnOpen.IsEnabled=false;
 			gridPanel.Visibility = Visibility.Visible;
@@ -39,20 +62,35 @@ namespace Dynamic
 			const double targetTranslate = start_width - end_width;
 			var animScale = new DoubleAnimation((isOpen ? 1 : targetScale),
 			                                    (isOpen ?  targetScale : 1),
-			                                TimeSpan.FromSeconds(seconds));
+			                                    TimeSpan.FromSeconds(seconds));
 			var animTranslate = new DoubleAnimation((isOpen ? 0 : targetTranslate),
 			                                        (isOpen ? targetTranslate : 0),
-			                                TimeSpan.FromSeconds(seconds));
+			                                        TimeSpan.FromSeconds(seconds));
 			animScale.Completed += animate_Completed;
 			scale.BeginAnimation(ScaleTransform.ScaleXProperty, animScale);
 			translate.BeginAnimation(TranslateTransform.XProperty, animTranslate);
 		}
 		private void animate_Completed(object sender, EventArgs e)
 		{
+			animate_Completed();
+		}
+		private void animate_Completed()
+		{
+			if (!isOpen) {
+				List<Bar> _bar = new List<Bar>();
+
+				_bar.Add(new Bar() { BarName=DateTime.Today.AddMonths(-1).ToString("dd MMM yy"),Value=2200.5});
+				_bar.Add(new Bar() { BarName = DateTime.Today.AddDays(-20).ToString("dd MMM yy"), Value = 1206.6 });
+				_bar.Add(new Bar() { BarName = DateTime.Today.AddDays(-8).ToString("dd MMM yy"), Value = 253.1 });
+				_bar.Add(new Bar() { BarName = DateTime.Today.AddDays(-5).ToString("dd MMM yy"), Value = 808.8 });
+				
+				this.DataContext = new RecordCollection(_bar);
+			}
 			isOpen = !isOpen;
-			btnOpen.Content = isOpen ? "Close" : "Open";
+			btnOpen.Content = isOpen ? "Close" : "Delivery";
 			gridPanel.Visibility = isOpen ? Visibility.Visible : Visibility.Collapsed;
 			btnOpen.IsEnabled=true;
 		}
+		#endregion
 	}
 }
